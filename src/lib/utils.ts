9f5de197eb8a229f03426057ed4ab0eff88085d7
danielbuva <daniel.valdecantos@gmail.com>
2025-08-17 23:22:48 -0700

    feat: add tunnel rat

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,9 @@
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
+import tunnelRat from 'tunnel-rat'
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs))
 }
+
+export const tunnel = tunnelRat()
